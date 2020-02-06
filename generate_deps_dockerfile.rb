5ce2b5f56d358b03a49431353f35bc8d5390a91b
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-02-06 19:55:52 +0200

    removed old files

--- generate_deps_dockerfile.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-# Generates a new docker file with the dependencies listed in it. Note: it's not the same
-# as the Dockerfile in this folder, but it is a useful reference for updating it.
-require_relative 'RubySetupSystem/DockerImageCreator.rb'
-
-require_relative 'bc_dependencies.rb'
-
-runDockerCreate(@libs_list, nil)
