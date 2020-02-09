398efeaec30453ad95bf0bcf9f8d6f0bebce0e7a
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-02-09 12:21:03 +0200

    Fixed badge count display

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -5,8 +5,7 @@ class BadgesController < ApplicationController
   def index
     @badges = current_user.granted_badges if user_signed_in?
 
-    @total_granted = GrantedBadge.count
+    @total_granted = GrantedBadge.all.count
     @total_users = GrantedBadge.all.group(:user_id).count.length
-    logger.info("total is: #{@total_users}")
   end
 end
