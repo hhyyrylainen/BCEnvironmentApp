b0bb9c168d607a23c18c47fecd61f7feccae797f
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2020-02-09 12:35:45 +0200

    Added granted date to badges list

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -3,7 +3,7 @@
 # Shows users badges, total badges of all users, as well as info on the badges
 class BadgesController < ApplicationController
   def index
-    @badges = current_user.granted_badges if user_signed_in?
+    @badges = current_user.granted_badges.order(created_at: :desc) if user_signed_in?
 
     @total_granted = GrantedBadge.all.count
     @total_users = GrantedBadge.all.group(:user_id).count.length
